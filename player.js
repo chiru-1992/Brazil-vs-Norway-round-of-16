c9714d2ae77aaaba0989112aca2682150075b32b
chiru-1992 <chirugurung730@gmail.com>
2026-07-05 10:17:36 +0530

    Create player.js with player information
    Added player data for Brazil and Norway.

---
+++ player.js
@@ -0,0 +1,38 @@
+const players = [
+{
+id:1,
+brazil:{
+name:"Alex Sandro",
+image:"players/alex_sandro.jpg"
+},
+norway:{
+name:"Erling Haaland",
+image:"players/erling_haaland.jpg"
+}
+},
+
+{
+id:2,
+brazil:{
+name:"Bremer",
+image:"players/bremer.jpg"
+},
+norway:{
+name:"Martin Odegaard",
+image:"players/martin_odegaard.jpg"
+}
+},
+
+{
+id:3,
+brazil:{
+name:"Danilo",
+image:"players/danilo.jpg"
+},
+norway:{
+name:"Alexander Sorloth",
+image:"players/alexander_sorloth.jpg"
+}
+}
+
+];
